636a6955f39a3b329f6a97f6a506b72d3897d771
Karla <karla.alvarez@michelada.io>
2020-05-14 17:47:36 -0500

    API request first triying

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import Login from './View/Login.js'
-import Projects from './View/Projects';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { projectsInfo, tasksInfo, userInfo } from './librery/functionsApiCalls';
+import logo from './logo.svg';
+import './App.css';
 
-const App =() => {
+function App() {
+  //const dataProjects = projectsInfo();
+  const datatasks = tasksInfo();
+  //const dataUser = userInfo();
   return (
-    <BrowserRouter>
-    <Switch>
-  <Route exact path="/" component={Login} />
-  <Route path="/Projects" component={Projects} />
-</Switch>
-    </BrowserRouter>)
+    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
