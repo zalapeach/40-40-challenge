33d28c8908d3fa71b7984f0b0acce9581b098610
yaidi <yaidi2001@gmail.com>
2020-05-27 22:35:43 -0500

    I moved the counter file and change the imports of this file

--- src/Components/SelectStatus.js
+++ src/Components/SelectStatus.js
@@ -5,10 +5,10 @@ const SelectStatus = (selectStatus) =>{
 return(
 <select className='custom-select' id='inputGroupSelect02'>
                 <option selected> {selectStatus + ' '} cambiar</option>
-                <option defaultValue='1' className='High'> Completado</option>
-                <option defaultValue='2' className='Medium'>Revisión</option>
-                <option defaultValue='3' className='Low'> Detenido por cliente</option>
-                <option defaultValue='4' > Pruebas</option>
+                <option defaultValue='1' className='Complete'> Completado</option>
+                <option defaultValue='2' className='Review'>Revisión</option>
+                <option defaultValue='3' className='Stop'> Detenido por cliente</option>
+                <option defaultValue='4' className='Testing'> Pruebas</option>
             </select>
 )
 
